9808ce96fe424682a76aebfc31df4846184b9a33
coolassassin <coolstreetassassin@gmail.com>
2022-05-24 23:14:45 +0400

    fix types

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import * as React from 'react';
 import micromemoize from 'micro-memoize';
 
-export const useCallbackFactory = (callback: () => any, deps: React.DependencyList | undefined) => React.useMemo(() => micromemoize(callback), deps)
+export const useCallbackFactory = <T extends (...args: Array<any>) => any>(callback: T, deps: React.DependencyList | undefined): T => React.useMemo(() => micromemoize(callback), deps)
